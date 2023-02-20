30001b07fbbfecb9321c2f85790df9d147b6dcf1
connor <connor.alexander@my.uwrf.edu>
2023-02-19 21:27:34 -0600

    commit

---
+++ src/data/FeedbackData.js
@@ -0,0 +1,19 @@
+const FeedbackData = [
+    {
+        id: '1',
+        rating: 10,
+        text: 'Lorem ipsum dolor sit, amet consectetur adipisicing elit. Laboriosam magni enim quos officiis libero dignissimos neque cum, officia optio quia!',
+    },
+    {
+        id: '2',
+        rating: 9,
+        text: 'Lorem ipsum dolor sit amet consectetur adipisicing elit. Ipsa accusamus facilis exercitationem dolores consequuntur? Consectetur explicabo ratione maxime assumenda totam?',
+    },
+    {
+        id: '3',
+        rating: 6,
+        text: 'Lorem ipsum dolor sit amet consectetur adipisicing elit. Blanditiis consequatur hic sapiente odio eveniet! Fugit.',
+    },
+];
+
+export default FeedbackData;
